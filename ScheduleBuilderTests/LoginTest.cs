4af149e4ee620524cb59e84560a97d0ec2a4549b
Megan Brown <mjones29@my.westga.edu>
2019-06-23 18:33:26 -0400

    Created Login unit test to confirm user is login and redirected

---
+++ ScheduleBuilderTests/LoginTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using ScheduleBuilder.Controllers;
+using ScheduleBuilder.Model;
+using System;
+using System.Web.Mvc;
+using Xunit;
+
+namespace ScheduleBuilderTests
+{
+    public class LoginTest
+    {
+
+        public Person person = new Person
+        {
+            LastName = "Doe",
+            FirstName = "Jane",
+            Ssn = "123456789",
+            Gender = "Female",
+            DateOfBirth = DateTime.Now,
+            RoleId = 1,
+            StatusId = 1,
+            Email = "jdoe@email.com",
+            StreetAddress = "3163 Boogieland",
+            Zipcode = "30263",
+            Phone = "6785443445",
+            Password = "pass",
+            Username = "test"
+        };
+
+        [Fact]
+        public void ShouldLogUserIn()
+        {
+            HomeController home = new HomeController();
+            ActionResult result = home.Login(person);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotInstanceOfType(result, typeof(RedirectToRouteResult));
+        }
+    }
+}
