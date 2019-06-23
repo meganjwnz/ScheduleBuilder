df26cf00972f370d7a542e910736e22cac2a2ce0
dcoleman90 <dcolem12@my.westga.edu>
2019-06-23 12:41:55 -0400

    testCommit

--- ScheduleBuilderTests/PersonTests.cs
+++ ScheduleBuilderTests/PersonTests.cs
@@ -42,6 +42,7 @@ namespace ScheduleBuilderTests
             Assert.Equal(expected, actual);
         }
 
+        //Comment added for commit
         [Fact]
         public void TestShouldReturnFirstName()
         {
