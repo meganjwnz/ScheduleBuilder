3e1354767bce90796437b24e73d653a3b60f226c
Megan Brown <mjones29@my.westga.edu>
2019-06-26 10:49:13 -0400

    Created CRUD Position pages with position controller without routing or functionality

---
+++ Controllers/PositionController.cs
@@ -0,0 +1,86 @@
+﻿
+using System.Web.Mvc;
+
+namespace ScheduleBuilder.Controllers
+{
+    public class PositionController : Controller
+    {
+        // GET: Position
+        public ActionResult Positions()
+        {
+            return View();
+        }
+
+        // GET: Position/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Position/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Position/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Positions");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Position/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Position/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Position/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Position/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
