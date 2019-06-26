49403d4ef85bbd09b38a2c5ef25492d7719ab7bf
Megan Brown <mjones29@my.westga.edu>
2019-06-26 11:06:04 -0400

    Added display names for each position attribute in Position model

--- Model/Position.cs
+++ Model/Position.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,13 @@ namespace ScheduleBuilder.Model
 {
     public class Position
     {
+        [Display(Name = "Position ID")]
         public int positionID { get; set; }
+        [Display(Name = "Position Title")]
         public string positionTitle { get; set; }
+        [Display(Name = "Position Status")]
         public int? isActive { get; set; }
+        [Display(Name = "Position Description")]
         public string positionDescription { get; set; }
     }
 }
