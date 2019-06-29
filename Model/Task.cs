72bf13627f60f54be8689c2441a67af3b8a45461
Megan Brown <mjones29@my.westga.edu>
2019-06-29 18:44:45 -0400

    Created Task Model and empty Task Controller

---
+++ Model/Task.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ScheduleBuilder.Model
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+
+        public int Task_title { get; set; }
+
+        public int Task_description { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
