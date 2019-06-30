286be146ee5633b3feaff8f7c080faa8595bbef4
dcoleman90 <dcolem12@my.westga.edu>
2019-06-29 20:41:01 -0400

    Stupid Nav bar forgot where the controller action which never moved changed names or really did anything other than be the best ActionResult it could possibly be, is it its fault that the Nav bar decided to forget it whose to say.  Yet - After much frustration Nav bar found its long lost love and they are happy again - NO idea why but I'd like to think that their View Control relationship will be all the stronger for it
    - Added Displaynames in Shift
    - Fixed/modiefied when buttons and values in AddTimePunchPage would be displayed

--- Model/Shift.cs
+++ Model/Shift.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,35 +11,60 @@ namespace ScheduleBuilder.Model
     /// </summary>
     public class Shift
     {
+        
         //Shift Id
         public int shiftID { get; set; }
+        
         //Scheduled shift id
         public int scheduleShiftID { get; set; }
+        
         //Scheduled person Id
         public int personID { get; set;}
+        
         //Scheduled persons first name
+        [Display(Name = "First Name")]
         public string personFirstName { get; set; }
+       
         //Scheduled persons last name
+        [Display(Name = "Last Name")]
         public string personLastName { get; set; }
+        
         //Shift postion ID
         public int positionID { get; set; }
+        
         //Shift postition name
         public string positionName { get; set; }
+
         //Shift start time
+        [Display(Name = "Scheduled Start Time")]
         public DateTime scheduledStartTime { get; set; }
+
         //shift end time
+        [Display(Name = "Scheduled End Time")]
         public DateTime scheduledEndTime { get; set; }
+
         //shift lunch start time
+        [Display(Name = "Scheduled Break Start")]
         public DateTime? scheduledLunchBreakStart { get; set; }
+
         //shift lunch end time
+        [Display(Name = "Scheduled Break End")]
         public DateTime? scheduledLunchBreakEnd { get; set; }
+
         //shift actual start time
+        [Display(Name = "Actual Clock In")]
         public DateTime? actualStartTime { get; set; }
+
         //shift actual end time
+        [Display(Name = "Actual Clock Out")]
         public DateTime? actualEndTime { get; set; }
+
         //shift actual lunch start time
+        [Display(Name = "Actual Break Start")]
         public DateTime? actualLunchBreakStart { get; set; }
+        
         //shift actual lunch end time
+        [Display(Name = "Actual Break End")]
         public DateTime? actualLunchBreakEnd { get; set; }
 
     }
