a40ddbdc2e84dfe0da0d2e532bd7a7510d02f162
Megan Brown <mjones29@my.westga.edu>
2019-06-30 14:05:54 -0400

    Created PositionTaskDAL and Model.

---
+++ DAL/PositionTaskDAL.cs
@@ -0,0 +1,44 @@
+﻿
+
+using ScheduleBuilder.Model;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace ScheduleBuilder.DAL
+{
+    public class PositionTaskDAL
+    {
+        /// <summary>
+        /// Retrieves a list of all tasks that have a position assigned to it
+        /// </summary>
+        /// <returns></returns>
+        public List<PositionTask> GetAllPositionTasks()
+        {
+            SqlConnection connection = ScheduleBuilder_DB_Connection.GetConnection();
+            List<PositionTask> positionTaskList = new List<PositionTask>();
+
+            string selectStatement = "SELECT t.id, t.task_title, t.isActive, t.task_description " +
+                "FROM task t";
+
+            using (connection)
+            {
+                connection.Open();
+
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            PositionTask positionTask = new PositionTask();
+                            positionTask.TaskId = int.Parse(reader["taskId"].ToString());
+                            positionTask.PositionId = int.Parse(reader["roleId"].ToString());
+                            positionTaskList.Add(positionTask);
+                        }
+                    }
+                }
+            }
+            return positionTaskList;
+        }
+    }
+}
---
+++ Model/PositionTask.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace ScheduleBuilder.Model
+{
+    public class PositionTask
+    {
+        public int PositionId { get; set; }
+        public int TaskId { get; set; }
+
+    }
+}
